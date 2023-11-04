65168ae6c5020a71f8d19c140b506397890cd02e
Ranjay Bose <bosery@mail.uc.edu>
2023-11-03 20:22:17 -0400

    Created ParkDetails page and added Javascript snippet

---
+++ IS7024_01_23/Pages/ParkDetail.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using parksNamespace;
+using System.Net.Http;
+
+namespace IS7024_01_23.Pages
+{
+    public class ParkDetailModel : PageModel
+    {
+       
+        public async Task OnGet()
+        {
+           
+        }
+    }
+}
