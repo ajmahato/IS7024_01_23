cc70ed1d85e5dba93bef2e39f0ff1322f98a59e1
Nishit Raj <rajnt@mail.uc.edu>
2023-11-25 22:32:57 -0500

    feedback form updated

---
+++ IS7024_01_23/Pages/Feedback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024_01_23.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
