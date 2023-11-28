44c40928e84fbabc07ec7a57b2898c9ad96db98a
Ajay Mahato <ajmahato14@gmail.com>
2023-11-28 15:17:28 -0500

    updated API key

--- IS7024_01_23/Pages/StateList.cshtml.cs
+++ IS7024_01_23/Pages/StateList.cshtml.cs
@@ -6,42 +6,34 @@ namespace IS7024_01_23.Pages
 {
     public class StateListModel : PageModel
     {
-        private IList<StateDataModel> stateName = new List<StateDataModel>();
 
         public void OnGet()
         {
-            string url = $"https://neighborhoodwatch.azurewebsites.net/api/values";
-            using (HttpClient client = new HttpClient())
+            Task<List<StateDataModel>> stateName = GetData();
+            List<StateDataModel> stateDataModels = stateName.Result;
+            ViewData["StateList"]= stateDataModels;
+        }
+
+        private async Task<List<StateDataModel>> GetData()
+        {
+            List<StateDataModel> StateName = null;
+            try
             {
-                try
+                string url = "https://neighborhoodwatch.azurewebsites.net/api/values";
+                using (HttpClient client = new HttpClient())
                 {
-                    // Send GET request to the API
-                    HttpResponseMessage response = client.GetAsync(url).Result;
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Read the content as a string
-                        string apiResponse = response.Content.ReadAsStringAsync().Result;
-
-                        stateName = System.Text.Json.JsonSerializer.Deserialize<List<StateDataModel>>(apiResponse);
-
-                        // Split the response into a list based on a delimiter (if applicable)
-                        //stateName = apiResponse.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-                        ViewData["StateList"] = stateName;
-
-                    }
-                    else
-                    {
-                        throw new Exception($"API request failed with status code: {response.StatusCode}");
-                        // Handle non-success status codes
-                        // You can throw an exception, log an error, or handle it as needed
-                    }
-                }
-                catch (Exception ex)
-                {
-                    LogException(ex);
+                    HttpResponseMessage apiResponse = await client.GetAsync(url);
+                    apiResponse.EnsureSuccessStatusCode();
+                    var list = await apiResponse.Content.ReadAsStringAsync();
+                    StateName = StateDataModel.FromJson(list);
                 }
             }
+            catch(Exception ex)
+            {
+                ViewData["error"] = "The other team's API is not working. Please try visiting \n https://neighborhoodwatch.azurewebsites.net/api/values";
+                LogException(ex);
+            }
+            return StateName;
         }
         private void LogException(Exception ex)
         {
